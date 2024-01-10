129d990c83f30252419a750ea79e5983d4ed52b7
will street <will@williamdstreet.com>
2024-01-10 11:17:34 -0600

    changed message from Hello World to new message

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi, Hello, Well hello there, William Street, 1/10/2014");
+Console.WriteLine("Hi, Hello, Well hello there, William Street, 1/10/2014");
+
